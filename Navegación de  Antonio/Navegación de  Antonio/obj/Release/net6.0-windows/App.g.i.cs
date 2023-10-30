e0eb7e0ca22fa03ec168468918fa46551898366c
Alberto <agonzalber10@educacion.navarra.es>
2023-10-30 13:00:31 +0100

    Añadida imagen como recurso

--- Navegación de  Antonio/Navegación de  Antonio/obj/Release/net6.0-windows/App.g.i.cs
+++ Navegación de  Antonio/Navegación de  Antonio/obj/Release/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Navegación_de__Antonio {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Navegación_de__Antonio {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             Navegación_de__Antonio.App app = new Navegación_de__Antonio.App();
             app.InitializeComponent();
