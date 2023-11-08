229ed0226a11e7f5f57dede0e7b6141d6b55ef6e
AntonioDam1 <antonioaguidam1@gmail.com>
2023-11-08 21:02:32 +0100

    Avanzando con introduccion de datos

--- Introduccion de Datos/Introduccion de Datos/MainWindow.xaml.cs
+++ Introduccion de Datos/Introduccion de Datos/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace Introduccion_de_Datos
         {
             InitializeComponent();
         }
+
+
+        private void txt_GotFocus(Object sender, RoutedEventArgs e)
+        {
+            if(sender is TextBox textBox)
+            {
+                if(textBox.Text == "Direccion" || textBox.Text == "Ciudad" || textBox.Text == "Provincia" || textBox.Text == "Codigo Postal" || textBox.Text == "Pais")
+                {
+                    textBox.Text = "";
+                }
+            }
+        }
+
+        private void txt_LostFocus(Object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textBox) 
+            { 
+                if(String.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    if (textBox.Name == "txtDireccion")
+                    {
+                        textBox.Text = "Direccion";
+                    }
+                    else if (textBox.Name == "txtCiudad")
+                    {
+                        textBox.Text = "Ciudad";
+                    }
+                    else if (textBox.Name == "txtProvincia")
+                    {
+                        textBox.Text = "Provincia";
+                    }
+                    else if (textBox.Text == "txtPostal")
+                    {
+                        textBox.Text = "Codigo Postal";
+                    }
+                    else if (textBox.Text == "txtPais")
+                    {
+                        textBox.Text = "Pais";
+                    }
+                }
+            }
+        }
     }
 }
