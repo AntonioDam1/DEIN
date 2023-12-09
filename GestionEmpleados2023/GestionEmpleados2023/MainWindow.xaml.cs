78660b6c34eae6594a79d2c1cd3f84625f02afc9
AntonioDam2B <antonioaguidam1@gmail.com>
2023-12-10 00:37:36 +0100

    Terminado proyecto Gestion de empleados

---
+++ GestionEmpleados2023/GestionEmpleados2023/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GestionEmpleados2023
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListaEmpleados(object sender, RoutedEventArgs e)
+        {
+            ListaEmpleados lista = new ListaEmpleados();
+            this.Close();
+            lista.Show();
+        }
+
+        private void AgregarEmpleado(object sender, RoutedEventArgs e)
+        {
+            AgregarEmpleado agregar = new AgregarEmpleado();
+            this.Close();
+            agregar.Show();
+        }
+
+        private void Buscar(object sender, RoutedEventArgs e)
+        {
+            BuscarEmpleado buscar = new BuscarEmpleado();
+            this.Close();
+            buscar.Show();
+        }
+    }
+}
